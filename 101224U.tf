74574cb4284ac44a1ce8ea7ca6f0d2fa6cb93516
Abhijith SR <abhijithsrm@gmail.com>
2024-12-18 18:28:50 +0530

    new

--- 101224U.tf
+++ 101224U.tf
@@ -1,27 +1,26 @@
 provider "aws" {
-  region = "ap-south-1"  # Corrected to the region
+  region = "ap-south-1"  # Correct region for your resources
 }
 
-# Data source to reference the existing VPC by name (dc1024)
-data "aws_vpc" "dc1024" {
+# Data source to reference the existing VPC named 'vpcdec10'
+data "aws_vpc" "vpcdec10" {
   filter {
     name   = "tag:Name"
-    values = ["dc1024"]
+    values = ["vpcdec10"]
   }
 }
 
-# Subnet within the existing VPC
-resource "aws_subnet" "svpcdec10" {
-  vpc_id     = data.aws_vpc.dc1024.id
-  cidr_block = "10.0.0.0/25"  # Subnet within the VPC
-  availability_zone = "ap-south-1"  # Replace with your preferred AZ
-  tags = {
-    Name = "svpcdec10"
+# Data source to reference the existing Subnet named 'subdec10'
+data "aws_subnet" "subdec10" {
+  filter {
+    name   = "tag:Name"
+    values = ["subdec10"]
   }
 }
 
+# Security group definition
 resource "aws_security_group" "secgp" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = data.aws_vpc.vpcdec10.id  # Use existing VPC
 
   ingress {
     from_port   = 22
@@ -49,16 +48,22 @@ resource "aws_security_group" "secgp" {
   }
 }
 
+# Ubuntu EC2 instance definition
 resource "aws_instance" "ubuntu_instance" {
   ami           = "ami-053b12d3152c0cc71"  # Replace with the latest Ubuntu AMI ID
   instance_type = "t2.micro"
   key_name      = "ukeym"  # Replace with your key pair name
 
   vpc_security_group_ids = [aws_security_group.secgp.id]
-  subnet_id              = aws_subnet.my_subnet.id  # Correctly reference the subnet
-   associate_public_ip_address = true  # Enable auto-assigned public IP
+  subnet_id              = data.aws_subnet.subdec10.id  # Use existing subnet
+  associate_public_ip_address = true  # Enable auto-assigned public IP
+
   tags = {
     Name = "UDEC18"
   }
 }
 
+# Output the public IP of the instance
+output "instance_public_ip" {
+  value = aws_instance.ubuntu_instance.public_ip
+}
